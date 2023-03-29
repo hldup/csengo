116bed3e3ecdc6583c81d6abe68c8fff51442113
berryes <therealkispetofi@gmail.com>
2023-03-29 21:03:16 +0200

    Alpha 1.1.1

--- server/index.ts
+++ server/index.ts
@@ -7,16 +7,16 @@ import User from './models/users';
 import { checkSchema, validationResult } from 'express-validator';
 import Session from './models/sessions';
 import Om from './models/om';
-const crypto = require('crypto')
 import axios from 'axios';
+// defining app
 const app: Express = express()
 app.use(express.json());
 
 function random(len: number): string {
   let options = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
-  const token = Array.from(crypto.getRandomValues(new Uint32Array(len))) 
-  .map((x) => options[x as number % options.length])
-  .join('')
+  const token = Array.from(crypto.getRandomValues(new Uint32Array(len)))
+    .map((x) => options[x as number % options.length])
+    .join('')
   return token
 }
 require('dotenv').config()
@@ -30,7 +30,7 @@ const port = process.env.PORT;
     const db = await open({
       filename: './database.sqlite',
       driver: sqlite3.Database
-    }) 
+    })
     await db.close();
 
     await dbInit();
@@ -59,25 +59,27 @@ app.post('/login',
       isString: true,
       notEmpty: true,
     }
-    }),
-    async (req:Request, res:Response ) => {
+  }),
+  async (req: Request, res: Response) => {
     // error handeling for headers/formdata
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
     // cache useragent/ip and check if the requests sent in the past 10 minutes is less then 10
-    let user = await User.findOne({where: {username: req.headers.username }})
-    if(!user) return res.sendStatus(401); // in case no user with username exits
+    let user = await User.findOne({ where: { username: req.headers.username } })
+    if (!user) return res.sendStatus(401); // in case no user with username exits
     if (user.password != req.headers.password) return res.sendStatus(401); // in case password is incorrect
 
     // finding user session
-    let user_session = await Session.findOne({where: {
-      user: user.id 
-    } })
+    let user_session = await Session.findOne({
+      where: {
+        user: user.id
+      }
+    })
 
     // in case already has session
     if (user_session) return res.send(user_session.token);
-    
+
     user_session = await Session.create({
       user: user.id,
       token: random(128),
@@ -90,7 +92,7 @@ app.post('/login',
       token: user_session.token,
       expiry: user_session.expires
     })
-});
+  });
 
 app.post('/register',
   checkSchema({
@@ -108,11 +110,11 @@ app.post('/register',
       notEmpty: true
     },
   }),
-  async (req:Request, res:Response ) => {
+  async (req: Request, res: Response) => {
     // error handeling for headers/formdata
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-    
+
     // hcaptcha key validation
     let response = await axios({
       method: "GET",
@@ -122,20 +124,20 @@ app.post('/register',
         secret: process.env.HCAPTCHA_SECRET as string
       }
     })
-    if(!response.data.success) return res.sendStatus(401);
+    if (!response.data.success) return res.sendStatus(401);
 
 
-    let user = await User.findOne({where: {username: req.body.username }});
+    let user = await User.findOne({ where: { username: req.body.username } });
     // if user already exists return
     if (user) return res.sendStatus(201);
-    
-    let user_om = await Om.findOne({where: {id: req.body.om }});
+
+    let user_om = await Om.findOne({ where: { id: req.body.om } });
     // if the om id given by user is not in db return as 401 
-    if(!user_om) return res.sendStatus(401);
-    
-    user = await User.findOne({where: {om: req.body.om }});
+    if (!user_om) return res.sendStatus(401);
+
+    user = await User.findOne({ where: { om: req.body.om } });
     // if there is an user already with the om
-    if(user) return res.sendStatus(201);
+    if (user) return res.sendStatus(201);
 
     // creating user in DB
     user = await User.create({
@@ -145,7 +147,7 @@ app.post('/register',
       administrator: false,
     })
 
-    
+
     // creating a session for the user
     let user_session = await Session.create({
       user: user.id,
@@ -153,8 +155,8 @@ app.post('/register',
       // 2 week expiry
       expires: Date.now() + 1209600000
     });
-    
-     res.send({
+
+    res.send({
       token: user_session.token,
       expiry: user_session.expires
     })
