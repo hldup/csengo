bf570d50be509857849fea13d43e7d89aa430912
berryes <therealkispetofi@gmail.com>
2023-04-12 10:36:54 +0200

    3.5.1

--- server/init.ts
+++ server/init.ts
@@ -1,11 +1,9 @@
 import Om from "./models/om"
-import Session from "./models/sessions"
 import User from "./models/users"
 import Sound from './models/sound'
 import Vote from "./models/votes"
 const dbInit = async () => {
     await User.sync()
-    await Session.sync()
     await Om.sync();
     await Sound.sync()
     await Vote.sync()
