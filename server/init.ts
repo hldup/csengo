503c0df19b3be7ca0c93138e409763e05c5af783
TGyAkos <t.gyula.akos@gmail.com>
2023-09-07 20:07:51 +0200

    11/10 security fixed

--- server/init.ts
+++ server/init.ts
@@ -4,6 +4,7 @@ import Sound from "./models/sound";
 import Vote from "./models/votes";
 import Weekly from "./models/weekly";
 import Token from "./models/token";
+import Submit from "./models/submit";
 const dbInit = async () => {
 	await User.sync();
 	await Om.sync();
@@ -11,6 +12,7 @@ const dbInit = async () => {
 	await Vote.sync();
 	await Weekly.sync();
 	await Token.sync();
+	await Submit.sync();
 };
 
 export default dbInit;
---
+++ server/routes/submit.route.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+
+router.post(
+    "/",
+
+    async (req: Request, res: Response) => {
+
+    }
+);
+
+module.exports = router;
